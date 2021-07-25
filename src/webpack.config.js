93f48874d5022a37c2be8c91cd5d6993b66642b2
dede_20191130 <1044adad@gmail.com>
2021-07-25 22:10:40 +0900

    compile for including ie11

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -39,6 +39,7 @@ const config = {
     rules: [
       {
         test: /\.(js|jsx)$/i,
+        exclude: path.resolve(__dirname, 'node_modules'),
         loader: "babel-loader",
       },
       {
@@ -55,6 +56,7 @@ const config = {
       },
     ],
   },
+  target: ['web', 'es5'],
 };
 
 module.exports = () => {
